c6cbcfa96c40dc6574cfb00e5eac87d8f412db44
Ndahimana Bonheur <bonheurndahimana125@gmail.com>
2023-09-04 11:30:54 +0200

    Receiving the request and send response as file or JSON

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
 const http = require('http')
 const path = require('path')
 const fs = require('fs')
+const url = require('url')
 
 const server = http.createServer((req,res) => {
-    console.log(req.url)
-    res.write('Hello from port to browser...')
-    res.end()
+    if (req.url === '/') {
+        fs.readFile(
+            path.join(__dirname,'public','index.html'),
+            (err, content) => {
+                if(err) throw err
+                res.writeHead(200, {
+                    'Content-Type' : 'text/html'
+                })
+                res.end(content)
+            }
+        )
+    }
+    if (req.url === '/about') {
+        fs.readFile(
+            path.join(__dirname,'public','about.html'),
+            (err, content) => {
+                if(err) throw err
+                res.writeHead(200, {
+                    'Content-Type' : 'text/html'
+                })
+                res.end(content)
+            }
+        )
+    }
+
+    if (req.url == '/api/users') {
+        const users = [
+            {
+                'names' : 'Bonheur NDAHIMANA',
+                'age' : 10
+            },
+            {
+                'names' : 'Amuli NTIHINYUKA',
+                'age' : 18
+            }
+        ]
+        res.writeHead(200, {
+            'Content-Type' : 'application/JSON'
+        })
+        res.end(JSON.stringify(users))
+    }
 })
 
 const PORT = process.env.PORT || 5000
