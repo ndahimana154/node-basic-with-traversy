d318c3b3f9c24f4c89e2326e986b958450e154ad
Ndahimana Bonheur <bonheurndahimana125@gmail.com>
2023-09-04 09:35:50 +0200

    Http 1

---
+++ references/http_demo.js
@@ -0,0 +1,9 @@
+const http = require('http')
+
+// Create server object
+http.createServer((req, res) => {
+    //Write message 
+    res.write('Hello world')
+    res.end()
+})
+.listen(5000, () => console.log("server running...."))
